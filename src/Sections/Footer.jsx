0ac9f9b83396e783db9f40f09fe5dc9016eb2020
Cecilia Suarez <cecisuarez08@gmail.com>
2024-11-11 11:43:33 -0300

    Linked from logo to home

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div style={{display: 'flex', justifyContent:'space-around', width: '100vw', padding: '0%'}}>
-        <h1>LOGO</h1>
+        <Link to={"/"}><h1>LOGO</h1></Link>
         <Navbar/>
         <div style={{display: 'flex', justifyContent:'space-around', padding: '0%'}}>
             <h4>linkedin</h4>
--- src/Sections/Header.jsx
+++ src/Sections/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-around', width: '100vw', padding: '0%' }}>
-        <h1>LOGO</h1>
+        <Link to={"/"}><h1>LOGO</h1></Link>
         <Navbar/>
         <div style={{ display: 'flex', justifyContent: 'space-around'}}>
             <h3>🌐</h3>
